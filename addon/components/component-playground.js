20c9b61a1964d6b413c166c11a0c3c42f031946d
Dan Hedgecock <hedgecock.d@gmail.com>
2016-10-04 14:26:03 -0700

    Set up project files

---
+++ addon/components/component-playground.js
@@ -0,0 +1,7 @@
+import Ember from 'ember';
+import hbs from 'htmlbars-inline-precompile';
+const { Component } = Ember;
+
+export default Component.extend({
+  layout: hbs`<p>component-playground</p>`
+});
---
+++ app/components/component-playground.js
@@ -0,0 +1,1 @@
+export { default } from 'ember-component-playground/components/component-playground';
