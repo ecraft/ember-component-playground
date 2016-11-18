a208b235c28ee5b87ffd919538fa863630ddd126
Dan Hedgecock <hedgecock.d@gmail.com>
2016-11-18 08:08:14 -0800

    Use ember-cli-codemirror-shim to pull in codemirror from npm instead of bower

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-/* jshint node: true */
 'use strict';
 
 module.exports = {
@@ -7,21 +6,26 @@ module.exports = {
   /*
    * Pull in template compiler and set default codemirror configurations for
    * consuming app.
-   * TODO: Does this work when this is a nested addon?
    */
   included: function(app) {
     this._super.included.apply(this, arguments);
+    this.ui.writeLine('Configuring ember-component-playground');
+
+    let codemirrorConfig = ((this.project.config(process.env.EMBER_ENV) || {}).codemirror || {});
+
+    // see: https://github.com/ember-cli/ember-cli/issues/3718
+    while (typeof app.import !== 'function' && app.app) {
+      app = app.app;
+    }
+
+    // Set resources on Addon instances
+    this.app = app;
 
     // Required to compile templates at runtime
-    if (app.import) {
-      app.import('bower_components/ember/ember-template-compiler.js');
-    }
+    app.import('bower_components/ember/ember-template-compiler.js');
 
     // Provide default codemirror configuration
-    if (app.options && (!app.options.codemirror || !app.options.codemirror.modes)) {
-      app.options.codemirror = {
-        modes: ['handlebars']
-      };
-    }
+    if (!codemirrorConfig.modes) { codemirrorConfig.modes = ['handlebars']; }
+    if (!codemirrorConfig.themes) { codemirrorConfig.themes = ['panda-syntax']; }
   }
 };
